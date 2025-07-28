172fbd172ca645df1a65a3c34cdea2e6fcd45f5d
Farhanunnasih <farhanunnasih@gmail.com>
2025-07-28 12:44:03 +0700

    build: Add soft-delete

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Registration extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
         'name',
         'email',
--- app/Models/TwilioLog.php
+++ app/Models/TwilioLog.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TwilioLog extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'message_sid',
         'account_sid',
